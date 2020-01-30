685cf563a64cec492df8c357dab0454a675fe4b5
Student <example@example.com>
2020-01-30 10:39:08 -0800

    add destination url for the FE

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -10,64 +10,32 @@ class DestinationsController < ApplicationController
   # GET /destinations/1
   # GET /destinations/1.json
   def show
+    @destination = Destination.find(params[:id])
+    response = HTTP.get(//localhost:3001/destinations)
   end
 
-  # GET /destinations/new
-  def new
-    @destination = Destination.new
-  end
-
-  # GET /destinations/1/edit
-  def edit
-  end
-
-  # POST /destinations
-  # POST /destinations.json
   def create
-    @destination = Destination.new(destination_params)
-
-    respond_to do |format|
-      if @destination.save
-        format.html { redirect_to @destination, notice: 'Destination was successfully created.' }
-        format.json { render :show, status: :created, location: @destination }
-      else
-        format.html { render :new }
-        format.json { render json: @destination.errors, status: :unprocessable_entity }
-      end
-    end
+    @destination = Destination.create!(destination_params)
   end
 
-  # PATCH/PUT /destinations/1
-  # PATCH/PUT /destinations/1.json
   def update
-    respond_to do |format|
-      if @destination.update(destination_params)
-        format.html { redirect_to @destination, notice: 'Destination was successfully updated.' }
-        format.json { render :show, status: :ok, location: @destination }
-      else
-        format.html { render :edit }
-        format.json { render json: @destination.errors, status: :unprocessable_entity }
-      end
+    @destination = Destination.find(params[:id])
+    if @destination.update!(destination_params)
+      render status: 200, json: {
+        message: "This destination has been updated successfully."
+      }
     end
   end
 
-  # DELETE /destinations/1
-  # DELETE /destinations/1.json
   def destroy
-    @destination.destroy
-    respond_to do |format|
-      format.html { redirect_to destinations_url, notice: 'Destination was successfully destroyed.' }
-      format.json { head :no_content }
+    @destination = Destination.find(params[:id])
+    if @destination.destroy
+      render status: 200, json: {
+        message: "This destination has been deleted successfully."
+      }
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_destination
-      @destination = Destination.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def destination_params
       params.fetch(:destination, {})
     end

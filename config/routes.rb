d97f9325ac560d30d28e042317c406c9bee826c5
Student <example@example.com>
2020-01-30 16:39:25 -0800

    add FE for travel api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   root "destinations#index"
 
-  resources :destinations
+  resources :destinations do
+    resources :reviews
+  end
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/account/:id' => 'users#show', :as => :user
+  get '/account/:id/edit' => 'users#edit', :as => :edit_user
+  patch '/account/:id' => 'users#patch'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end
